ec12617d347c80eab2ec760b5bfa0e81bdf0ec72
Kikiriki070204 <kiarabarrientossalazar@gmail.com>
2025-07-30 16:59:16 -0600

    Actividad y diseño de Login y Registro

---
+++ app/src/main/java/com/example/booksmartapp/models/Usuario.java
@@ -0,0 +1,87 @@
+package com.example.booksmartapp.models;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellido_paterno;
+    private String apellido_materno;
+    private String correo;
+    private String contraseña;
+    private boolean active;
+    private int rol_id;
+    public Usuario(int id, String nombre, String apellido_paterno, String apellido_materno, String correo, String contraseña, boolean active, int rol_id) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido_paterno = apellido_paterno;
+        this.apellido_materno = apellido_materno;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.active = active;
+        this.rol_id = rol_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    public String getApellido_materno() {
+        return apellido_materno;
+    }
+
+    public void setApellido_materno(String apellido_materno) {
+        this.apellido_materno = apellido_materno;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getRol_id() {
+        return rol_id;
+    }
+
+    public void setRol_id(int rol_id) {
+        this.rol_id = rol_id;
+    }
+
+}
